d6e4dfc27ba83a9dd7db623aff75c2faed9f6bf4
HotDogCat <g8xk7su3@gmail.com>
2023-04-18 18:54:16 +0800

    Update 2023-04-18T10:54:14.132Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,16 +1,15 @@
-console.log('Hello!');
+"use strict";
 
-$(document).ready(() => {
+console.log('Hello!');
+$(document).ready(function () {
   console.log('HesSchool Hello!');
 });
-
-document.getElementById("myButton1").addEventListener("click", function() {
+document.getElementById("myButton1").addEventListener("click", function () {
   document.getElementById("div1").style.display = "none";
   document.getElementById("div2").style.display = "none";
 });
-
-
-document.getElementById("myButton2").addEventListener("click", function() {
+document.getElementById("myButton2").addEventListener("click", function () {
   document.getElementById("div1").style.display = "block";
   document.getElementById("div2").style.display = "block";
 });
+//# sourceMappingURL=all.js.map
