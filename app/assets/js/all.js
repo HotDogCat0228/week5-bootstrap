380351d8a9326f062022869afd587ada46633228
HotDogCat <g8xk7su3@gmail.com>
2023-04-18 18:52:29 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,16 @@
+console.log('Hello!');
+
+$(document).ready(() => {
+  console.log('HesSchool Hello!');
+});
+
+document.getElementById("myButton1").addEventListener("click", function() {
+  document.getElementById("div1").style.display = "none";
+  document.getElementById("div2").style.display = "none";
+});
+
+
+document.getElementById("myButton2").addEventListener("click", function() {
+  document.getElementById("div1").style.display = "block";
+  document.getElementById("div2").style.display = "block";
+});
